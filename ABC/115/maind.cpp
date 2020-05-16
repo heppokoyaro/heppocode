2f5c8f7608257591660cf63766d27f744f9b7e8b
heppokoyaro <62793023+heppokoyaro@users.noreply.github.com>
2020-05-16 23:24:16 +0900

    今日の精進

--- ABC/115/maind.cpp
+++ ABC/115/maind.cpp
@@ -6,11 +6,47 @@ using ll = long long;
 static const int INF = 1000000000;
 static const ll MOD = 1000000007;
 
- 
+ll a[51];
+ll p[51];
+
+ ll solve(ll n, ll x){
+   ll ans = 0;
+   if(n == 0){
+     ans += 1;
+   }
+   else if(x == 1){
+     ans += 0;
+   }
+   else if(x <= 1 + a[n-1]){
+     ans += solve(n - 1, x - 1);
+   }
+   else if(x == 2 + a[n-1]){
+     ans += p[n-1] + 1;
+   }
+   else if(x <= 2 + 2 * a[n-1]){
+     ans += p[n-1] + 1 + solve(n - 1, x - 2 - a[n-1]);
+   }
+   else{
+     ans += 2 * p[n-1] + 1;
+   }
+   return ans;
+ }
 
 int main(){
   cin.tie(nullptr);
   ios_base::sync_with_stdio(false);
-  
+  ll n, x;
+  cin >> n >> x;
+
+  a[0] = 1;
+  p[0] = 1;
+  rep(i, 51){
+    a[i+1] = a[i] * 2 + 3;
+    p[i+1] = p[i] * 2 + 1;
+  }
+
+  ll ans = solve(n, x);
+
+  cout << ans << endl;
   return 0;
 }
